90e6274d2170261526ca13b21b67ea22b795970d
KseOss <104015053+KseOss@users.noreply.github.com>
2024-12-07 09:42:41 +0300

    Update Class1.cs

--- PracticalEight/Class1.cs
+++ PracticalEight/Class1.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace Pr8
 {
-    public class Bus : IAuto, IPassenger, ICloneable, IComparable
+    public class Bus : IAuto, IPassenger, ICloneable, IComparable //Класс Bus реализует несколько интерфейсов
     {
         public string ModelBus {  get; set; }
         public int SeatPassenger {  get; set; }
@@ -17,16 +17,18 @@ namespace Pr8
         { 
             return $"Модель автобуса - {ModelBus} и количество вмещаемых пассажиров - {SeatPassenger} ";
         }
-        public int CompareTo(object obj)
+        public int CompareTo(object obj)//реализация интерфейса
+            //позволяет сравнивать объекты с другими объектами
         {
-            Bus temp = obj as Bus;
-            if (temp != null) 
+            Bus temp = obj as Bus; //Принимает объект obj, который затем приводится к типу Bus
+            if (temp != null) //Елси obj не является null и успешно приводится к типу Bus, производится сравнение количества мест между текущим объектом и переданным метсом
             {
                 return this.SeatPassenger.CompareTo(temp.SeatPassenger);
             }
             return 0;
+            //метод возращает положительное значение, если текущий объект имеет больше мест; отрицательное, если меньше мест; ноль если количество мест одинаковое
         }
-        public object Clone()
+        public object Clone() //создает и возвращает новый экземпляр класса Bus, копируя значения свойство ModelBus и SeatPassanger текущего обхекта
         {
             return new Bus(this.ModelBus, this.SeatPassenger);
         }
